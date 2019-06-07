c16670462c5216583c45014ba1b2c36fc29732e5
Dan Moneypenny <catullus1973@gmail.com>
2019-06-06 21:39:47 -0700

    fix index

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+const moment = require('moment');
 const request = require('request');
 const fs = require('fs');
 const appKey = process.env.DR_KEY;
 const content = '<html><body>test</body></html>';
+
+const testName = `test.${moment().format('DD.MM.hh.mm.ss')}`;
 
 config = {
   url: 'https://docraptor.com/docs',
@@ -15,7 +18,7 @@ config = {
     doc: {
       document_content: content,
       type: 'pdf',
-      name: 'test1234',
+      name: testName,
       test: false,
       prince_options: {
         media: 'print',
@@ -29,8 +32,9 @@ request.post(config, function(err, response, body) {
   if(err) {
     console.log(err.message);
   }
-  fs.writeFile('test12344.pdf', body, 'binary', function(writeErr) {
-    console.log(response.status);
+  fs.writeFile(`OUTPUT/${testName}`, body, 'binary', function(writeErr) {
+    console.log(`Saved: ${testName}`);
   });
 });
+
 
