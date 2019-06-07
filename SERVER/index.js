d4e3019b8c346d87d8f27e1f4e0efa66bce275b9
Dan Moneypenny <catullus1973@gmail.com>
2019-06-06 21:03:44 -0700

    update script

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,2 +1,36 @@
-require('dotenv').config()
+require('dotenv').config();
+const request = require('request');
+const fs = require('fs');
+const appKey = process.env.DR_KEY;
+const content = '<html><body>test</body></html>';
+
+config = {
+  url: 'https://docraptor.com/docs',
+  encoding: null,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  json: {
+    user_credentials: appKey,
+    doc: {
+      document_content: content,
+      type: 'pdf',
+      name: 'test1234',
+      test: false,
+      prince_options: {
+        media: 'print',
+        profile: 'PDF/X-1a:2001'
+      }
+    }
+  }
+};
+
+request.post(config, function(err, response, body) {
+  if(err) {
+    console.log(err.message);
+  }
+  fs.writeFile('test12344.pdf', body, 'binary', function(writeErr) {
+    console.log(response.status);
+  });
+});
 
