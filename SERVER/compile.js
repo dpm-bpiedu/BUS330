d3ff04b711d3c74c8b45a7257a2dfd031f49d212
Dan Moneypenny <catullus1973@gmail.com>
2019-06-10 00:05:56 -0700

    update

--- SERVER/compile.js
+++ SERVER/compile.js
@@ -1,23 +1,40 @@
+const request = require("request");
+const fs = require("fs");
+const cheerio = require("cheerio");
+const args = process.argv.slice(2)
 
-const request = require('request');
-const fs = require('fs');
-const cheerio = require('cheerio');
+chapters = ['frontmatter', '01', '02', '03', '04','05','06','06','07','08','09'];
 
+const chapter = args[0];
 
+const urlStem =
+  "https://ne.edgecastcdn.net/0004BA/constellation/testing/pdf/BUS330/all/chapter_";
 
-request('https://ne.edgecastcdn.net/0004BA/constellation/testing/pdf/BUS330/all/chapter_01.html', function(err, response, html) {
-
+request(`${urlStem}${chapter}.html`, function(error, response, html) {
   const $ = cheerio.load(html);
   const body = $('body').html();
   const bodyContent = $(body).html();
-  const section = `<section class='chapter01'>${body}</section>`
+  const section = `<section class='chapter01'>${body}</section>`;  
 
-  fs.writeFile('OUTPUT/test2.html', section, (err) => {
-    if(err) console.log(err);
-    console.log("new file");
-  })
+  fs.appendFile('OUTPUT/test.html', section, (err) => {
+    if(err) {
+      console.log(err);
+    }
+    console.log(`file ${chapter} appended`);
+  });
 
-});
+}
 
+//  request(`${urlStem}${chapter}.html`, function(err, response, html) {
 
+//   const $ = cheerio.load(html);
+//   const body = $('body').html();
+//   const bodyContent = $(body).html();
+//   const section = `<section class='chapter01'>${body}</section>`;
+
+//   fs.appendFile('OUTPUT/test2.html', section, (err) => {
+//     if(err) console.log(err);
+//     console.log("new file");
+//   });
+// }
 
